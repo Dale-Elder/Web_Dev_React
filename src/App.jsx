458635d136fbd54557599d58fd08a28b7f34b8ea
Dale <66420874+Dale-Elder@users.noreply.github.com>
2023-04-16 15:44:34 +0100

    added comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  // useState returns an array with two items: the new item and a function to set the new item
   const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
 
+  // handleSubmit is a function that takes an event as an argument
   function handleSubmit(e) {
     e.preventDefault();
-
+    // setTodos is a function that takes a function as an argument
     setTodos((currentTodos) => {
       return [
+        // currentTodos is an array of objects
         ...currentTodos,
+        // the new object has a random id, the title is the value of newItem, and completed is false
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
@@ -22,13 +26,17 @@ export default function App() {
     });
   }
 
+  // handleSubmit is a function that takes an event as an argument
   return (
+    // the fragment <> </> is a way to return multiple elements without wrapping them in a div
     <>
+      <h1 className="header">Todo List</h1>
       <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
           <input
             value={newItem}
+            // onChange takes an event as an argument and sets the value of newItem to the value of the input
             onChange={(e) => setNewItem(e.target.value)}
             type="text"
             id="item"
